e6c980d55666559ad5551adfcd9c5784228e5d78
91948 <91948@VARSHITHA>
2023-11-03 17:47:18 +0530

    first commit

---
+++ java/src/programs/thread.java
@@ -0,0 +1,25 @@
+package programs;
+import java.util.*;
+class thread_demo2 implements Runnable{
+	public void run() {
+		System.out.println("current thread id:"+Thread.currentThread().getId());
+	}
+}
+class thread_demo3 implements Runnable{
+	public void run() {
+		System.out.println("current thread id:"+Thread.currentThread().getId());
+	}
+}
+public class thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        thread_demo2 t1=new thread_demo2();
+        Thread t5=new Thread(t1);
+        t5.start();
+        thread_demo3 t2=new thread_demo3();
+        Thread t6=new Thread(t2);
+        t6.start();
+	}
+
+}
