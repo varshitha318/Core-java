ab37e3890248adbea738080c1063fe95a5db3a26
91948 <91948@VARSHITHA>
2023-11-03 17:35:35 +0530

    first commit

---
+++ java/src/program/boxdemo.java
@@ -0,0 +1,19 @@
+package program;
+
+class Box
+{
+	double width;
+	double height;
+	double depth;
+}
+ class Boxdemo {
+public static void main(String args[]) {
+	Box mybox=new Box();
+	mybox.width=10;
+	mybox.height=20;
+	mybox.depth=30;
+	double vol;
+	vol=mybox.width*mybox.height*mybox.depth;
+	System.out.println("volume="+vol);
+}
+}
---
+++ java/src/program/inheritance.java
@@ -0,0 +1,26 @@
+package program;
+
+class Employee
+{  
+float salary=40000;  
+}  
+
+class b extends Employee{
+	String name="abc";
+}
+
+
+public class  inheritance  extends b
+{  
+int age=22;  
+public static void main(String args[])
+{  
+
+ inheritance  p=new  inheritance ();  
+ System.out.println("Programmer salary is:"+p.salary); 
+ System.out.println("name of Programmer is:"+p.name);  
+ System.out.println("age of Programmer is:"+p.age);  
+}  
+}
+
+
